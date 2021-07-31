9f8c1f02373f867d0308dc8344de5938149cb19f
Daniel Davtyan <danieldavtyan99@gmail.com>
2021-07-31 14:03:08 +0400

    Added distance calculation and added it to result list

--- Stars_ Filtering.py
+++ Stars_ Filtering.py
@@ -14,6 +14,18 @@ def my_split(string, delimiters= ' '):
         result.append(word)
     return result
 
+
+# it is a bubble sort
+def my_sort(sub_li):
+    l = len(sub_li)
+    for i in range(0, l):
+        for j in range(0, l-i-1):
+            if sub_li[j][3] > sub_li[j + 1][3]:
+                tempo = sub_li[j]
+                sub_li[j] = sub_li[j + 1]
+                sub_li[j + 1] = tempo
+    return sub_li
+
 RA_DEC = input('Please input equtorial cordinates(RA DEC)')
 RA_DEC_TUPLE = tuple(my_split(RA_DEC))
 FOV_H_V = input("Please enter HFOV and VFOV")
@@ -50,10 +62,25 @@ square_h_l = float(RA_DEC_TUPLE[1]) - (float(FOV_H_V_TUPLE[1])/2)
 square_h_r = float(RA_DEC_TUPLE[1]) + (float(FOV_H_V_TUPLE[1])/2)
 
 filtered_stars = []
-# going through ra/dec coordinates
+# going through ra/dec coordinates and filtering them
 for i in data:
     if (square_w_l <= float(i[0]) <= square_w_r) and (square_h_l <= float(i[1]) <= square_h_r):
         filtered_stars.append(i)
 
-for i in filtered_stars:
+my_sort(filtered_stars)
+result = []
+i = 0
+
+# distance calculation
+while i < NUMBER_OF_STARS:
+    result.append(filtered_stars[i])
+
+    distance = pow(float(filtered_stars[i][0]) - float(RA_DEC_TUPLE[0]), 2) + \
+                               pow(float(filtered_stars[i][1]) - float(RA_DEC_TUPLE[1]), 2)
+    result[i].append(distance)
+    i = i + 1
+
+for i in result:
     print(i)
+
+
